a0efe9cce568621249121b3a7f6664e8b5eb2807
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-02-05 07:23:07 -0300

    Criação das categorias

--- src/data.js
+++ src/data.js
@@ -2,22 +2,40 @@ export const sliderItems = [
     {
         id: 1,
         img: "/images/ge_disjuntor_01.png",
-        title: "DISJUNTORES", 
-        desc:  "Disjuntor para chuveiro elétrico, fiação de casas e apartamentos e demais eletrodomésticos",
+        title: "DISJUNTORES",
+        desc: "Disjuntor para chuveiro elétrico, fiação de casas e apartamentos e demais eletrodomésticos",
         bg: "2e3092",
     },
     {
         id: 2,
         img: "/images/cobrecon_cabo_flexivel_01.png",
-        title: "CABOS", 
+        title: "CABOS",
         desc: "Cabos de todo tipo de milímetro para sua residência ou comércio",
-        bg: "2e3092",
+        bg: "#FDE2F0",
     },
     {
         id: 3,
         img: "/images/lorenzetti_ducha_03.png",
-        title: "CHUVEIROS", 
+        title: "CHUVEIROS",
         desc: "Diversas marcas e modelos de chuveiros Lorezentti",
-        bg: "2e3092",
-    }
-]
+        bg: "#FCE5DC",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "/images/ge_disjuntor_01.png",
+        title: "DISJUNTORES",
+    },
+    {
+        id: 2,
+        img: "/images/cobrecon_cabo_flexivel_01.png",
+        title: "CABOS",
+    },
+    {
+        id: 3,
+        img: "/images/lorenzetti_ducha_03.png",
+        title: "CHUVEIROS",
+    },
+];
