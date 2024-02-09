4466714caa7a40adfa9c7012370b72ac8dcc86cf
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-02-08 23:33:47 -0300

    Adicionando produtos

--- src/data.js
+++ src/data.js
@@ -24,20 +24,207 @@ export const sliderItems = [
     },
 ];
 
-export const categories = [
+export const popularProducts = [
     {
         id: 1,
         img: "/images/ge_disjuntor_01.png",
-        title: "DISJUNTORES",
     },
     {
         id: 2,
         img: "/images/cobrecon_cabo_flexivel_01.png",
-        title: "CABOS",
     },
     {
         id: 3,
         img: "/images/lorenzetti_ducha_03.png",
-        title: "CHUVEIROS",
+    },
+    {
+        id: 4,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 5,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 6,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 7,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 8,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 9,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 10,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 11,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 12,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 13,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 14,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 15,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 16,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 17,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 18,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 19,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 20,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 21,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 22,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 23,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 24,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 25,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 26,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    ,
+    {
+        id: 27,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 28,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 29,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 30,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 31,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 32,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 33,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 34,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 35,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 36,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 37,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 38,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 39,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 40,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 41,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 42,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 43,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 44,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 45,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 46,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 47,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 48,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 49,
+        img: "/images/lorenzetti_ducha_03.png",
+    },
+    {
+        id: 50,
+        img: "/images/lorenzetti_ducha_03.png",
     },
 ];
+
