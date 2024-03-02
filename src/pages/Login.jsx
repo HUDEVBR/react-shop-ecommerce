2dee95c954f58d341236b2ede092b20ee677057a
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-03-02 17:44:20 -0300

    - Pagina de Login e Registro finalizadas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,78 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import logo from "../assets/images/background_jfmateriais.png"
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)),url(${logo}) center 80%;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+  border: solid 1px;
+  background-color: transparent;
+  color: #2e3092;
+  font-weight: 500;
+  &::placeholder{
+    color: #c5c5c5; 
+    font-weight: 700;
+  }
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #85bc18;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
 
 const Login = () => {
   return (
-        <Container>
-          
-        </Container>
+    <Container>
+    <Wrapper>
+        <Title>LOGAR</Title>
+        <Form>
+            <Input placeholder="Usuário" />
+            <Input placeholder="Senha" />
+          <Button>LOGIN</Button>
+          <Link>ESQUECEU SEU SENHA?</Link>
+          <Link>CRIAR NOVA CONTA</Link>
+        </Form>
+    </Wrapper>
+  </Container>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,33 +1,66 @@
 import styled from "styled-components";
 import logo from "../assets/images/background_jfmateriais.png"
 
-
 const Container = styled.div`
     width: 100vw;
     height: 100vh;
-    background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.2)),url(${logo}) center;
-    background-size: 100vw;
-    background-repeat: no-repeat;
+    background: linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)),url(${logo}) center 80%;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+  border: solid 1px;
+  background-color: transparent;
+  color: #2e3092;
+  font-weight: 500;
+  &::placeholder{
+    color: #c5c5c5; 
+    font-weight: 700;
+  }
+`;
 
-const Agreement = styled.span``;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
 
-const Button = styled.button``;
-const Image = styled.img``;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #85bc18;
+  color: white;
+  cursor: pointer;
+`;
 
 const Register = () => {
   return (
         <Container>
           <Wrapper>
-              <Title>Crie sua conta</Title>
+              <Title>CRIE SUA CONTA</Title>
               <Form>
                   <Input placeholder="Nome" />
                   <Input placeholder="Sobrenome" />
@@ -38,7 +71,7 @@ const Register = () => {
                   <Agreement>Ao estar criando esta conta, Eu concedo o processamento dos meus
                       dados pessoais de acordo com a <b>POLÍTICA DE PRIVACIDADE</b> e a <b>LGPD</b>.
                   </Agreement>
-                  <Button>Criar</Button>
+                  <Button>CRIAR</Button>
               </Form>
           </Wrapper>
         </Container>
