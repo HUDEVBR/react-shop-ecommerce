eaf0c676cee97656967df3dc4ae53a76788aaa10
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-03-08 01:04:58 -0300

    Terminando de finalizar a página carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import styled from 'styled-components'
 import Footer from '../components/Footer';
+import { Add, Remove } from '@material-ui/icons';
 
 const Container = styled.div``;
 const Wrapper = styled.div`
@@ -16,12 +17,16 @@ const Top = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding: 20px;
 `;
 
 const TopButton = styled.button`
     padding: 10px;
     font-weight: 600;
     cursor: pointer;
+    border: ${(props) => props.type === "filled" && "none"};
+    background-color: ${(props) => props.type === "filled" ? "black" : "transparent"};
+    color: ${(props) => props.type === "filled" && "white"};
 `;
 
 const TopTexts = styled.div``;
@@ -31,8 +36,86 @@ const TopText = styled.span`
     margin: 0 10px;
 `;
 
-const Bottom = styled.div``;
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const Info = styled.div`
+    flex: 3;
+`;
 
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`;
+const Image = styled.img`
+    width: 200px;
+`;
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.span`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+`;
+
+const PriceDetail = styled.span`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`;
+
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+`;
+
+const Summary = styled.div`
+    flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+`;
+
+const SummaryTitle = styled.h1``;
+const SummaryItem = styled.div``;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const SummaryButton = styled.button``;
 
 const Cart = () => {
   return (
@@ -49,9 +132,69 @@ const Cart = () => {
                           <TopText>Lampadas (2)</TopText>
                           <TopText>Itens desejados (0)</TopText>
                       </TopTexts>
-                      <TopButton>FINALIZAR COMPRAS</TopButton>
+                      <TopButton type="filled">FINALIZAR COMPRAS</TopButton>
                   </Top>
-                  <Bottom></Bottom>
+                  <Bottom>
+                      <Info>
+                          <Product>
+                              <ProductDetail>
+                                  <Image src="./images/taschibra_luminaria_01.png"/>
+                                  <Details>
+                                      <ProductName><b>Produto:</b> Luminaria Taschibra </ProductName>
+                                      <ProductId><b>ID:</b> 002</ProductId>
+                                      <ProductColor color="black"/>
+                                  </Details>
+                              </ProductDetail>
+                              <PriceDetail>
+                                  <ProductAmountContainer>
+                                      <Add />
+                                      <ProductAmount>2</ProductAmount>
+                                      <Remove/>
+                                  </ProductAmountContainer>
+                                  <ProductPrice>R$ 30</ProductPrice>
+                              </PriceDetail>
+                          </Product>
+                          <Hr/>
+                          <Product>
+                              <ProductDetail>
+                                  <Image src="./images/starret_broca_01.png"/>
+                                  <Details>
+                                      <ProductName><b>Produto:</b> Broca Starret </ProductName>
+                                      <ProductId><b>ID:</b> 002</ProductId>
+                                      <ProductColor color="black"/>
+                                  </Details>
+                              </ProductDetail>
+                              <PriceDetail>
+                                  <ProductAmountContainer>
+                                      <Add />
+                                      <ProductAmount>1</ProductAmount>
+                                      <Remove/>
+                                  </ProductAmountContainer>
+                                  <ProductPrice>R$ 20</ProductPrice>
+                              </PriceDetail>
+                          </Product>
+                      </Info>
+                      <Summary>
+                          <SummaryTitle>RESUMO DE PEDIDOS</SummaryTitle>
+                          <SummaryItem>
+                              <SummaryItemText>Subtotal</SummaryItemText>
+                              <SummaryItemPrice>R$ 50</SummaryItemPrice>
+                          </SummaryItem>
+                          <SummaryItem>
+                              <SummaryItemText>Frete</SummaryItemText>
+                              <SummaryItemPrice>R$ 10</SummaryItemPrice>
+                          </SummaryItem>
+                          <SummaryItem>
+                              <SummaryItemText>Desconto com Frete</SummaryItemText>
+                              <SummaryItemPrice>R$ -4</SummaryItemPrice>
+                          </SummaryItem>
+                          <SummaryItem>
+                              <SummaryItemText type="total">Total</SummaryItemText>
+                              <SummaryItemPrice>R$ 56</SummaryItemPrice>
+                          </SummaryItem>
+                          <SummaryButton>FINALIZAR COMPRA</SummaryButton>
+                      </Summary>
+                  </Bottom>
               </Wrapper>
               <Footer/>
         </Container>
