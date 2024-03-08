6e2a13f31fe1e09161d25e1f7ba8942222363e3a
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-03-08 19:35:53 -0300

    Terminado a parte do carrinho, criando responsiviidade

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -109,13 +109,33 @@ const Summary = styled.div`
     border: 0.5px solid lightgray;
     border-radius: 10px;
     padding: 20px;
+    height: 60vh;
 `;
 
-const SummaryTitle = styled.h1``;
-const SummaryItem = styled.div``;
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props => props.type === "total" && "500"};
+    font-size: ${props => props.type === "total" && "24px"};
+`;
+
+
 const SummaryItemText = styled.span``;
 const SummaryItemPrice = styled.span``;
-const SummaryButton = styled.button``;
+
+
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+`;
 
 const Cart = () => {
   return (
@@ -188,11 +208,11 @@ const Cart = () => {
                               <SummaryItemText>Desconto com Frete</SummaryItemText>
                               <SummaryItemPrice>R$ -4</SummaryItemPrice>
                           </SummaryItem>
-                          <SummaryItem>
-                              <SummaryItemText type="total">Total</SummaryItemText>
+                          <SummaryItem type="total">
+                              <SummaryItemText>Total</SummaryItemText>
                               <SummaryItemPrice>R$ 56</SummaryItemPrice>
                           </SummaryItem>
-                          <SummaryButton>FINALIZAR COMPRA</SummaryButton>
+                          <Button>FINALIZAR COMPRA</Button>
                       </Summary>
                   </Bottom>
               </Wrapper>
